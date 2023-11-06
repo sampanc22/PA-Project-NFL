201cbe813aaebc56a35805a3a050fc7bd956c009
sampanc22 <sampanc22@gmail.com>
2023-11-06 14:05:20 -0500

    Updated web scraping for quarterbacks with rankings Add web scraping for running backs with rankings

--- qb_data_scraping.py
+++ qb_data_scraping.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
 
+# set up Beautiful Soup for quarterback data
 homepage = "https://www.pro-football-reference.com/years/2023/passing.htm"
 url = urlopen(homepage)
 stats = BeautifulSoup(url)
@@ -12,6 +13,7 @@ col_headers = [i.getText() for i in col_headers.findAll('th')]
 rows = stats.find_all('tr')[1:]
 qb_data = []
 
+# parse through data of quarterbacks and add to a list
 for i in range(35):
     qb_data.append([col.getText() for col in rows[i].findAll('td')])
 
@@ -19,14 +21,17 @@ qb_df = pd.DataFrame(qb_data, columns=col_headers[1:])
 qb_df.columns.values[-6] = 'YdsSk'
 
 # filter out irrelevant statistics (such as non-aggregated stats)
-relevant_stats = ['Yds', 'Cmp%', 'TD%', 'Int%', 'Succ%', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Rate', 'QBR', 'Sk%']
+relevant_stats = ['Yds', 'Cmp%', 'TD%', 'Int%', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Rate', 'QBR', 'Sk%']
 qb_df = qb_df[['Player', 'Tm', 'Age'] + relevant_stats]
 
+# convert data objects to numerical values
 for i in relevant_stats:
     qb_df[i] = pd.to_numeric(qb_df[i])
 
-qb_df = qb_df.query('Yds > 1000')
+# filter out quarterbacks with less than 1000 passing yards
+qb_df = qb_df.query('Yds >= 1000')
 
+# rank each quarterback according to every statistic
 for column in qb_df:
     if column == 'Player' or column == 'Tm' or column == 'Age':
         continue
---
+++ rb_data_scraping.py
@@ -0,0 +1,47 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# set up Beautiful Soup for running back data
+homepage = "https://www.pro-football-reference.com/years/2023/rushing.htm"
+url = urlopen(homepage)
+stats = BeautifulSoup(url)
+
+col_headers = stats.find_all('tr')[1]
+col_headers = [i.getText() for i in col_headers.findAll('th')]
+
+rows = stats.find_all('tr')[2:]
+rb_data = []
+
+# parse through data of running backs and add to a list
+for i in range(50):
+    if i != 0 and i % 29 == 0:
+        continue
+    rb_data.append([col.getText() for col in rows[i].findAll('td')])
+
+rb_df = pd.DataFrame(rb_data, columns=col_headers[1:])
+
+# filter out irrelevant statistics
+relevant_stats = ['G', 'GS', 'TD', 'Y/A', 'Y/G', 'Fmb']
+rb_df = rb_df[['Player', 'Tm', 'Age', 'Pos'] + relevant_stats]
+
+# convert data objects to numerical values
+for i in relevant_stats:
+    rb_df[i] = pd.to_numeric(rb_df[i])
+
+# filter out non-running backs and running backs with less than 5 GS
+rb_df = rb_df.query('GS >= 5')
+rb_df = rb_df[rb_df['Pos'] == 'RB']
+
+# rank each running back according to every statistic
+for column in rb_df:
+    if column == 'Player' or column == 'Tm' or column == 'Age' or column == 'Pos' or column == 'G' or column == 'GS':
+        continue
+    elif column == 'Fmb':
+        rb_df[column + ' Rank'] = rb_df[column].rank(ascending=True).astype(int)
+    else:
+        rb_df[column + ' Rank'] = rb_df[column].rank(ascending=False).astype(int)
+
+print(rb_df.to_string())
+
+
