3d19a75559079255c82c42f35e9be912b481a6ca
sampanc22 <sampanc22@gmail.com>
2023-11-06 12:19:42 -0500

    Updated web scraping for quarterbacks with rankings

--- qb_data_scraping.py
+++ qb_data_scraping.py
@@ -12,13 +12,28 @@ col_headers = [i.getText() for i in col_headers.findAll('th')]
 rows = stats.find_all('tr')[1:]
 qb_data = []
 
-for i in range(41):
-    if i % 29 != 0:
-        qb_data.append([col.getText() for col in rows[i].findAll('td')])
+for i in range(35):
+    qb_data.append([col.getText() for col in rows[i].findAll('td')])
 
 qb_df = pd.DataFrame(qb_data, columns=col_headers[1:])
 qb_df.columns.values[-6] = 'YdsSk'
+
 # filter out irrelevant statistics (such as non-aggregated stats)
-qb_df.drop(columns=["QBrec", "Lng", "Pos", "1D", "4QC", "GWD", "NY/A",
-                    "G", "GS", "Cmp", "Att", "Yds", "TD", "Int", "Sk", "YdsSk", "ANY/A"], inplace=True)
+relevant_stats = ['Yds', 'Cmp%', 'TD%', 'Int%', 'Succ%', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Rate', 'QBR', 'Sk%']
+qb_df = qb_df[['Player', 'Tm', 'Age'] + relevant_stats]
+
+for i in relevant_stats:
+    qb_df[i] = pd.to_numeric(qb_df[i])
+
+qb_df = qb_df.query('Yds > 1000')
+
+for column in qb_df:
+    if column == 'Player' or column == 'Tm' or column == 'Age':
+        continue
+    elif column == 'Int%' or column == 'Sk%':
+        qb_df[column + ' Rank'] = qb_df[column].rank(ascending=True).astype(int)
+    else:
+        qb_df[column + ' Rank'] = qb_df[column].rank(ascending=False).astype(int)
+
 print(qb_df.to_string())
+
